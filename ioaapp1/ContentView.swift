b708422af0164692307fb472637ab250d977ecb3
Andrew Owren <aowren21@gmail.com>
2023-05-21 17:02:28 -0700

    Initial Commit

---
+++ ioaapp1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ioaapp1
+//
+//  Created by user241078 on 5/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Image("Image")
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ ioaapp1/ioaapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  ioaapp1App.swift
+//  ioaapp1
+//
+//  Created by user241078 on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ioaapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
